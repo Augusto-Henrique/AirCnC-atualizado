408671117d27c5d56b7f530b2811dab54690d6ba
Augusto-Henrique <agostohsmiranda@gmail.com>
2025-04-17 01:17:34 -0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+import Rotas from './routes';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnc" />
+      <div className="content">
+        <Rotas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
